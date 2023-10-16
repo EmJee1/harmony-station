21482b21eae02ea51e714a68710eb1f49c11ef57
EmJee <mart-janroeleveld@outlook.com>
2023-10-16 14:52:01 +0200

    feat: add custom karla font

--- renderer/App.vue
+++ renderer/App.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="w-full bg-slate-100 p-4">
-    <Container class="flex items-center gap-8">
-      <ButtonIcon is="RouterLink" to="/">
-        <HomeIcon />
-      </ButtonIcon>
-      <Search class="w-full" />
-      <ButtonIcon is="RouterLink" to="/settings">
-        <CogIcon />
-      </ButtonIcon>
+  <div class="font-serif">
+    <div class="w-full bg-slate-100 p-4">
+      <Container class="flex items-center gap-8">
+        <ButtonIcon is="RouterLink" to="/">
+          <HomeIcon />
+        </ButtonIcon>
+        <Search class="w-full" />
+        <ButtonIcon is="RouterLink" to="/settings">
+          <CogIcon />
+        </ButtonIcon>
+      </Container>
+    </div>
+    <Container class="mt-8">
+      <RouterView />
+      <CurrentlyPlayingBar />
     </Container>
+    <FullscreenLoader v-if="fullscreenLoaderActive" />
   </div>
-  <Container class="mt-8">
-    <RouterView />
-    <CurrentlyPlayingBar />
-  </Container>
-  <FullscreenLoader v-if="fullscreenLoaderActive" />
 </template>
 
 <script lang="ts" setup>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./renderer/**/*.vue'],
   theme: {
+    fontFamily: {
+      serif: ['"Karla"', '"sans-serif"'],
+    },
     extend: {
       container: {
         center: true,
