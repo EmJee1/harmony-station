8904183f73e7375df8c3c0d0a773fd818a90ed49
EmJee <mart-janroeleveld@outlook.com>
2023-10-20 13:46:55 +0200

    feat: no-result component in queue

---
+++ renderer/components/NoResults.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col items-center justify-center gap-6">
+    <CubeTransparentIcon class="h-12 w-12" />
+    <Typography is="p" variant="heading-3">{{ title }}</Typography>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CubeTransparentIcon from '@heroicons/vue/24/outline/CubeTransparentIcon'
+import Typography from './Typography.vue'
+
+defineProps<{
+  title: string
+}>()
+</script>
--- renderer/views/Queue.vue
+++ renderer/views/Queue.vue
@@ -1,14 +1,17 @@
 <template>
   <Typography is="h1" variant="heading-2">Queue</Typography>
   <TrackTable
+    v-if="queue.length"
     :tracks="queue"
     :columns="['play-queue-track', 'title', 'year']"
     class="mt-6"
   />
+  <NoResults v-else title="The queue is empty" class="" />
 </template>
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
+import NoResults from '../components/NoResults.vue'
 import Typography from '../components/Typography.vue'
 import TrackTable from '../components/TrackTable.vue'
 import { useQueueStore } from '../stores/queue-store'
