77e2efc57b62f01a44bf5946453a7a1f8fe38350
EmJee <mart-janroeleveld@outlook.com>
2023-11-23 21:10:16 +0100

    feat: add back-button in topbar

--- renderer/App.vue
+++ renderer/App.vue
@@ -2,6 +2,13 @@
   <div class="font-serif">
     <div class="w-full bg-slate-100 p-4">
       <Container class="flex items-center gap-8">
+        <ButtonIcon
+          is="button"
+          :disabled="route.path === '/'"
+          @click="router.back"
+        >
+          <ChevronLeftIcon />
+        </ButtonIcon>
         <ButtonIcon is="RouterLink" to="/">
           <HomeIcon />
         </ButtonIcon>
@@ -21,23 +28,27 @@
 </template>
 
 <script lang="ts" setup>
+import ChevronLeftIcon from '@heroicons/vue/24/outline/ChevronLeftIcon'
 import HomeIcon from '@heroicons/vue/24/outline/HomeIcon'
 import CogIcon from '@heroicons/vue/24/outline/CogIcon'
 import { storeToRefs } from 'pinia'
 import { onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import ButtonIcon from './components/ButtonIcon.vue'
 import Container from './components/Container.vue'
 import CurrentlyPlayingBar from './components/CurrentlyPlayingBar.vue'
 import FullscreenLoader from './components/FullscreenLoader.vue'
 import Search from './components/Search.vue'
 import ToastProvider from './components/ToastProvider.vue'
+import { useContextMenuResponses } from './composables/context-menu-responses'
 import { useSettingsStore } from './stores/settings-store'
 import { useFullscreenLoaderStore } from './stores/fullscreen-loader-store'
-import { useContextMenuResponses } from './composables/context-menu-responses'
 
 useContextMenuResponses()
 const { fetchSettings } = useSettingsStore()
 const { fullscreenLoaderActive } = storeToRefs(useFullscreenLoaderStore())
+const router = useRouter()
+const route = useRoute()
 
 onMounted(async () => {
   await Promise.all([fetchSettings()])
